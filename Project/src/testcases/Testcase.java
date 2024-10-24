87efc60f557b07bf83845d475be7b0ddb4828cdc
sai <sai@DESKTOP-I05SNBG>
2024-10-24 19:55:43 +0530

    test

---
+++ Project/src/testcases/Testcase.java
@@ -0,0 +1,24 @@
+package testcases;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Testcase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("chromedivber.chrome.driver", "");
+		
+		
+		ChromeOptions option = new ChromeOptions();
+		
+		option.addArguments("--remote-allow-origins*");
+		
+		WebDriver driver = new ChromeDriver(option);
+		
+		driver.get("http://www.google.com");
+
+	}
+
+}
