92a8618a42ec4250e6f50cacce275979838d32ec
Aravind <Aravind@DESKTOP-OVC0NH1>
2025-04-05 21:18:09 +0530

    hello world

---
+++ Project/src/helloworld.java
@@ -0,0 +1,12 @@
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String test = "Hello Aravind";
+		System.out.println(test);
+	}
+
+}
